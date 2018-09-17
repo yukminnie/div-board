28961530baa1640c47d6979d99fc7488566e5e21
yukminnie <dadiaomin@126.com>
2018-09-17 17:40:25 +0800

    标记实现不同状态下的绘画

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 var div = document.getElementById('canvas')
 
+var painting = false
+
 div.onmousedown = function(a){
   console.log('点击了')
+  painting = true
   var x = a.clientX
   var y = a.clientY
   var divA = document.createElement('div')
@@ -10,10 +13,18 @@ div.onmousedown = function(a){
   div.appendChild(divA)
 }
 
-div.onmousemove = function(b){
-
+div.onmousemove = function(a){
+  if (painting){
+    console.log('点击了')
+    var x = a.clientX
+    var y = a.clientY
+    var divA = document.createElement('div')
+    divA.className = 'dot'
+    divA.style = 'left: ' + (x - 3) + 'px;' + 'top: ' + (y - 3) + 'px;'
+    div.appendChild(divA)
+  }
 }
 
 div.onmouseup = function(c){
-
+  painting = false
 }
